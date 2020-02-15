1db8a479ac21753613d131635c854b320d474b47
Bobby Gammill <bgammill@gmail.com>
2020-02-14 19:51:40 -0500

    Make it so exceptions stop any text from being copied. Bump version to 1.0.1.

--- clipboard-helper.js
+++ clipboard-helper.js
@@ -6,11 +6,10 @@ function copyToClipboard(text, html) {
             event.stopImmediatePropagation();
             event.preventDefault();
             event.clipboardData.setData("text/plain", link);
+            document.addEventListener("copy", oncopy, true);
+            document.execCommand("copy");
         } catch (e) {
             console.log('not base64');
         }
     }
-
-    document.addEventListener("copy", oncopy, true);
-    document.execCommand("copy");
 }
